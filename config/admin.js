7e39d75933053f74280ec10a92993cf661c4688c
HenryXu2020 <130348876+HenryXu2020@users.noreply.github.com>
2025-11-24 12:44:07 +0800

    Add apiToken and transfer token salt configurations
    Added apiToken and transfer token salt configurations to admin settings.

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,12 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'), // 这就是解决第一个警告的关键配置[citation:2]
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'), // 这个可以一并解决第二个警告
+    },
+  },
 });
